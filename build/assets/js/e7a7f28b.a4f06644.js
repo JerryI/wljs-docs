d1eb1d11e432779fb6a1e77892b12a1075a18510
Kirill <krikus.ms@gmail.com>
2024-08-21 17:06:29 +0200

    update

---
+++ build/assets/js/e7a7f28b.a4f06644.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwlx_docs=self.webpackChunkwlx_docs||[]).push([[1264],{8240:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/wljs-docs/blog/page/2","page":2,"postsPerPage":10,"totalPages":2,"totalCount":11,"previousPage":"/wljs-docs/blog","blogDescription":"Blog","blogTitle":"Blog title"}}')}}]);
